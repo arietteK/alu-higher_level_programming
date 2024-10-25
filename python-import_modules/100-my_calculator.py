4ce3d75a47a557c7b4eb685620a1cd0b1ff28a86
arietteK <a.rusaro@alustudent.com>
2024-10-25 03:56:49 -0700

    Build my own calculator!

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -18,5 +18,5 @@ if __name__ == "__main__":
     elif operator == '/':
         print(f"{a} {operator} {b} = {div(a, b)}")
     else:
-        print("Unknown operator. Only: +, -, * and / Available")
+        print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
