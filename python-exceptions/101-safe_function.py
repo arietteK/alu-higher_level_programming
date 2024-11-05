195835cca832db152349aeb32a5fdfa3e2808a05
arietteK <a.rusaro@alustudent.com>
2024-11-05 01:43:15 -0800

    Safe function

--- python-exceptions/101-safe_function.py
+++ python-exceptions/101-safe_function.py
@@ -7,5 +7,5 @@ def safe_function(fct, *args):
         result = fct(*args)
         return result
     except (ValueError, ZeroDivisionError, TypeError, IndexError):
-        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stdree)
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         return None
