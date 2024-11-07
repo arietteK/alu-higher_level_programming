e4645c8b8625d3309cd0df4d74065d593ee3d847
arietteK <a.rusaro@alustudent.com>
2024-11-07 01:26:05 -0800

    My first square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """
-This module defines an empty class called `Square` that can be used as a blueprint
+This module defines an empty class named `Square` that can be used as a layout
 for creating square objects.
 """
 
+
 class Square:
     """
-    This class represents an empty square.
-    
+    This class represents an empty square.    
     This class currently does not have any attributes or methods, and acts
     as the start of defining a square in future tasks.
     """
