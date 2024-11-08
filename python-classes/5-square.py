e688ffb8b5e4898cbaaff75bf77e6aecad51fecd
arietteK <a.rusaro@alustudent.com>
2024-11-08 02:17:23 -0800

    Printing a square

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-"""This module defines a class `Square` that represents a square"""
-
+"""
+This module defines a class `Square` that represents a square
+with a private instance attribute for its size.
+"""
 
 class Square:
     """ This class represents a square with a private attribute `size`."""
