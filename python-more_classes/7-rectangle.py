0a703a88b9afc277f41250815ecf32cf57e30234
arietteK <a.rusaro@alustudent.com>
2024-11-08 13:45:29 -0800

    Change representation

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -57,11 +57,11 @@ class Rectangle:
         return 2 * (self.__width + self.__height)
 
     def __str__(self):
-        """Returns a string representation of the rectangle using
-        the `#` character."""
+        """Returns a string representation of the rectangle 
+        using `print_symbol`"""
         if self.__width == 0 or self.__height == 0:
             return ("")
-        return "\n".join(["#" * self.__width for _ in range(self.__height)])
+        return "\n".join([str(self.print_symbol) * self.__width for _ in range(self.__height)])
 
     def __repr__(self):
         """Returns a string representation of the rectangle that can be used
