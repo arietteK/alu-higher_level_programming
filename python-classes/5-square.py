a8e9c6fcd815217e7d12304684de85f8a6045cf6
arietteK <a.rusaro@alustudent.com>
2024-11-08 02:12:24 -0800

    Printing a square

---
+++ python-classes/5-square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""This module defines a class `Square` that represents a square"""
+
+
+class Square:
+    """ This class represents a square with a private attribute `size`."""
+
+    def __init__(self, size=0):
+        """Initializes a new instance of the `Square` class."""
+
+        self.__size = size
+
+        """Getter for the private attribute size"""
+    @property
+    def size(self):
+        """property getter"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter property"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """A public method that returns the current square area"""
+        return(self.__size * self.__size)
+    def my_print(self):
+        """print repetition of square with # character"""
+        if self.__size == 0:
+            print()
+        for item i in range(self.__size):
+            [print("#", end='') for j in range(self.__size)]
+            print()
