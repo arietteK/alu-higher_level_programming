c1f59d7cbe45a8788af5127d0c93fc6b59316ea1
arietteK <a.rusaro@alustudent.com>
2024-11-09 13:56:11 -0800

    Integer validator

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -12,12 +12,12 @@ class BaseGeometry:
         """
         Public instance method that raises an exception indicating
         that the method is not yet implemented."""
-        raise NotImplementedError("area() is not implemented")
+        raise Exception("area() is not implemented")
     
     def integer_validator(self, name, value):
         """
         Validates that the given value is an integer greater than 0"""
         if not isinstance(value, int):
-            raise TypeError(f"{name} must be an integer")
+            raise TypeError(f"<name> must be an integer")
         if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+            raise ValueError(f"<name> must be greater than 0")
