b9e13e1cdd5a0372c9a7f0f2ef5dd58e699a066f
arietteK <a.rusaro@alustudent.com>
2024-11-13 13:30:09 -0800

    Write to a file

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Defining a write_file function with two arguments"""
+
+
+def write_file(filename="", text=""):
+    """Writes a string to a UTF-8 text file and returns the number of characters written."""
+    with open(filename, 'w', encoding='utf-8') as f:
+        return f.write(text)
