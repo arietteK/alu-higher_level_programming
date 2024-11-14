c7552c6d825b67b0dd2d8c18314ec8ce69207266
arietteK <a.rusaro@alustudent.com>
2024-11-14 13:05:05 -0800

    Student to JSON with filter

---
+++ python-input_output/10-student.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Contains the class Student"""
+
+
+class Student:
+    """Defines a student with first name, last name, and age."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes a new Student instance.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Retrieves a dictionary representation of a Student instance."""
+        if attrs is not None and isinstance(attrs, list):
+            return {key: value for key, value in self.__dict__.items() if key in attrs}
+
+        return self.__dict__
