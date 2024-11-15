2292ca0d4d18d5e5f1a190b9803fc32899f07861
arietteK <a.rusaro@alustudent.com>
2024-11-15 13:40:11 -0800

    My privileges!

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,7 +1,4 @@
--- List privileges for user_0d_1
-SELECT 'Grants for user_0d_1@localhost';
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
+-- List the privileges for user_0d_1 and user_0d_2
 
--- List privileges for user_0d_2
-SELECT 'Grants for user_0d_2@localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
