aafae8785d8039938a6875fd54568e9335e459b8
arietteK <a.rusaro@alustudent.com>
2024-11-15 02:40:12 -0800

    List by best

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,4 @@
+-- This script lists all records from 'second_table', displaying 'score' and 'name'.
+-- Records are ordered by 'score' in descending order (top first).
+
+SELECT score, name FROM second_table ORDER BY score DESC;
