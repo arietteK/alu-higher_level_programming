3a1e50298f8b942f7f27bac2a6c28ef43ec86c0d
arietteK <a.rusaro@alustudent.com>
2024-11-15 03:26:20 -0800

    Go to UTF8

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,5 +1,6 @@
 -- This script converts the database, table, and field to UTF-8 (utf8mb4) encoding
 -- with utf8mb4_unicode_ci collation.
+USE hbtn_0c_0;
 
 ALTER DATABASE hbtn_0c_0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 
