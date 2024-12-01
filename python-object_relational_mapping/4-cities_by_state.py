e75b50e91bd39cc55c9df9114bf06c92d31bb856
arietteK <a.rusaro@alustudent.com>
2024-12-01 22:16:34 +0200

    pycodestyle validation

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -9,8 +9,8 @@ if __name__ == "__main__":
                            passwd=argv[2], db=argv[3], charset="utf8")
     cu = conn.cursor()
     cu.execute("SELECT cities.id, cities.name, states.name FROM cities "
-                "JOIN states ON cities.state_id = states.id "
-                "ORDER BY cities.id ASC")
+               "JOIN states ON cities.state_id = states.id "
+               "ORDER BY cities.id ASC")
     query_rows = cu.fetchall()
     for row in query_rows:
         print(row)
