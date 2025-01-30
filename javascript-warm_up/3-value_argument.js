b51eeee33cf7462dd3b79a7cb5b4774527fd7691
arietteK <a.rusaro@alustudent.com>
2025-01-30 22:30:13 +0200

    fixed

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -2,8 +2,8 @@
 /* a script that prints the first argument passed to it */
 const args = process.argv;
 
-if (args[2] === undefined){
-    console.log('No argument');
-} else{
-    console.log(args[2]);
+if (args[2] === undefined) {
+  console.log('No argument');
+} else {
+  console.log(args[2]);
 }
