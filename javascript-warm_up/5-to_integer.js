cffee649047349d81f02eafa651b72c314b68a96
arietteK <a.rusaro@alustudent.com>
2025-01-30 22:41:16 +0200

    fixed

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
-/*prints My number: <first argument converted in integer>
+/* prints My number: <first argument converted in integer>
 if the first argument can be converted to an integer */
 const args = process.argvs[2];
+
 if (args === undefined || isNaN(parseInt(args))) {
-    console.log('Not a number');
+  console.log('Not a number');
 } else{
-    console.log('My number: ' + parseInt(args));
+  console.log('My number: ' + parseInt(args));
 }
