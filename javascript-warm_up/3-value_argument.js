19fcbf0af893941828cad957c45dff30bdd08a76
arietteK <a.rusaro@alustudent.com>
2025-01-30 22:14:18 +0200

    semistandards

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -3,7 +3,7 @@
 const args = process.argv;
 
 if (args[2] === undefined){
-    console.log("No argument");
+    console.log('No argument');
 } else{
     console.log(args[2]);
 }
--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -2,4 +2,5 @@
 /*prints two arguments passed to it, in the following format: “ is ” */
 const arg1 = process.argv[2];
 const arg2 = process.argv[3];
+
 console.log(arg1 + " is " + arg2);
