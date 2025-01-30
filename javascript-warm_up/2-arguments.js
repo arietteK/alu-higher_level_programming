a1e242a76cfc92265a418b8331debed84865a3e2
arietteK <a.rusaro@alustudent.com>
2025-01-30 20:53:39 +0200

    arguments

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 /*script that prints a message depending of the number of arguments passed*/
-const args = process.argv;
+const args = process.argv.slice(2);
 
 if (args.length === 0){
     console.log("No argument");
