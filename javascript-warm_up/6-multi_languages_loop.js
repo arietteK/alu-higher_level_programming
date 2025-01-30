a56d80686a23dfdb7c62d031e02a46446ac1f14e
arietteK <a.rusaro@alustudent.com>
2025-01-30 22:39:23 +0200

    fixed

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-/*prints 3 lines: (like 1-multi_languages.js) but by using
+/* prints 3 lines: (like 1-multi_languages.js) but by using
 an array of string and a loop */
 const lines = ['C is fun','Python is cool','JavaScript is amazing'];
 
 for (let i = 0; i < lines.length; i++) {
-    console.log(lines[i]);
+  console.log(lines[i]);
 }
--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -3,9 +3,9 @@
 const x = process.argv[2];
 
 if (x === undefined || isNaN(parseInt(x))) {
-    console.log('Missing number of occurrences');
+  console.log('Missing number of occurrences');
 } else {
     for ( let i = 0; i < parseInt(x); i++){
-        console.log('C is fun');
+      console.log('C is fun');
     }
 }
