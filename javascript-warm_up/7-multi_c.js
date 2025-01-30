a8f031e0421793a37aee974565838f587ab6313a
arietteK <a.rusaro@alustudent.com>
2025-01-30 22:45:53 +0200

    fixed

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -5,7 +5,7 @@ const x = process.argv[2];
 if (x === undefined || isNaN(parseInt(x))) {
   console.log('Missing number of occurrences');
 } else {
-    for ( let i = 0; i < parseInt(x); i++){
-      console.log('C is fun');
-    }
+  for (let i = 0; i < parseInt(x); i++) {
+    console.log('C is fun');
+  }
 }
