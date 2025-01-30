9ae1176a8f8c83368057840655f291ff22024f0a
arietteK <a.rusaro@alustudent.com>
2025-01-30 22:51:12 +0200

    trailing comma removed

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -3,7 +3,7 @@
 if the first argument can be converted to an integer */
 const args = process.argv[2];
 
-if (args === undefined || isNaN(parseInt(args,))) {
+if (args === undefined || isNaN(parseInt(args))) {
   console.log('Not a number');
 } else {
   console.log('My number: ' + parseInt(args));
